cf41d3fe77770ff6161819f6f2ca5d0b512c9cf2
jungjh123456 <jungjh123456@naver.com>
2020-11-06 14:13:15 +0900

    feat: random js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const $battle = document.querySelector('.battle');
 const $h3 = document.querySelector('.battle h3');
 let jobKey = '';
 let count = 0;
+
 //gender 선택 시작
 let genderChoice;
 //gender 선택 이벤트 핸들러 등록
@@ -19,11 +20,7 @@ $gender.onclick = e => {
   } else return; 
 }
 //gender 선택 끝
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
-}
+
 // next 버튼을 누르면 gender에 active 삭제, job에 active 추가하는 이벤트 핸들러
 $next.onclick = e => {
 
@@ -68,3 +65,27 @@ $battle.onclick = e => {
     count = 0;
   }
 }
+let newArr = [];
+let numArr = [];
+
+const randomItem = () => {
+  newArr = newArr.map((item, i, arr) => arr[numArr[i]]);
+  return newArr;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
+while (!(numArr.length === 16)){
+  while (1){
+    let num = getRandomInt(1, 17)
+    if(!numArr.includes(num)){
+     numArr.push(num);
+     break;
+    }   
+  }
+}
+
