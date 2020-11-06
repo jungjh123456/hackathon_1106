5eb1a3ca2751f3007c7f76ed7e2b3064813dbcc4
jungjh123456 <jungjh123456@naver.com>
2020-11-06 11:37:37 +0900

    fix: bug

--- js/app.js
+++ js/app.js
@@ -2,23 +2,32 @@ const $gender = document.querySelector('.gender');
 const $job = document.querySelector('.job');
 const $singer = document.querySelector('.singer');
 let jobKey = '';
+
+// 랜덤 함수
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
 }
 
-
+// job 온클릭
 $job.onclick = e => {
   if (e.target.matches('.job > .prev')) {
     $job.classList.remove('active');
     $gender.classList.add('active');
   } else if (e.target.matches('.job > .singer')){
-    jobkey = 'singer'
+    jobKey = 'singer'
   } else if (e.target.matches('.job > .actor')) {
-    jobkey = 'actor'
-  } else {
-    jobkey = 'all'
+    jobKey = 'actor'
+  } else if(e.target.matches('.job > .all')){
+    jobKey = 'all'
+  } else if(e.target.matches('.job > .start')){
+    if (jobKey === '') return;
+    console.log(jobKey);
   }
-  console.log(jobkey);
+
+
+
+
+  
 }
